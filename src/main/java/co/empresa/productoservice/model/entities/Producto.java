46202940edbb15c57a6254de55d17af7ba88b4a6
Alvaro Salazar <ah.salazar@uniandes.edu.co>
2025-03-18 13:32:15 -0500

    feat: Creacion de la entidad producto
    Se creo la entidad producto mapeada con JPA/Hibernate y con ayuda de Lombok para los getters y setters. Se uso la generacion de llaves primarias con estrategia Identity (Incrementa de a uno)

---
+++ src/main/java/co/empresa/productoservice/model/entities/Producto.java
@@ -0,0 +1,21 @@
+package co.empresa.productoservice.model.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+}
