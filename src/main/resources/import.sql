99fa118b43310d17184a91c10139e53510b5a791
Alvaro Salazar <ah.salazar@uniandes.edu.co>
2025-03-18 15:41:07 -0500

    feat: Datos de prueba en el import.sql
    Se agregaron datos de prueba en import.sql para que en el proceso DDL se construya la base de datos con dichos datos, ahorrando asi tiempo de desarrollo.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 1', 'Descripcion 1', 100);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 2', 'Descripcion 2', 200);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 3', 'Descripcion 3', 300);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 4', 'Descripcion 4', 400);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 5', 'Descripcion 5', 500);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 6', 'Descripcion 6', 600);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 7', 'Descripcion 7', 700);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 8', 'Descripcion 8', 800);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 9', 'Descripcion 9', 900);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 10', 'Descripcion 10', 1000);
