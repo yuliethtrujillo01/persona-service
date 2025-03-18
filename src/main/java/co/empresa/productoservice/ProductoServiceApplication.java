149f60aa4f4fa6c651877dcda044b811ca23fe52
Alvaro Salazar <ah.salazar@uniandes.edu.co>
2025-03-18 13:31:04 -0500

    chore: Inicializacion del proyecto con la estructura de paquetes del servicio
    Se creo una estructura por capas inspirada en arquitectura clasica.

---
+++ src/main/java/co/empresa/productoservice/ProductoServiceApplication.java
@@ -0,0 +1,13 @@
+package co.empresa.productoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/empresa/productoservice/ProductoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.empresa.productoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
